1689c4607d48996747adef9cafcab83a3a195355
nohakou <nohakousaiko@gmail.com>
2018-05-31 22:41:02 +0900

    Delete constants.js
    消そう

--- app/config/constants.js
+++
@@ -1,6 +0,0 @@
-export const FIREBASE_API_KEY = "AIzaSyAECwvH0VqOIBf3d4Ijaj_EC5DCPQNu7Sk";
-export const FIREBASE_AUTH_DOMAIN = "ffield-e616f.firebaseapp.com";
-export const FIREBASE_DATABASE_URL = "https://ffield-e616f.firebaseio.com";
-export const FIREBASE_PROJECT_ID = "ffield-e616f";
-export const FIREBASE_STORAGE_BUCKET = "ffield-e616f.appspot.com";
-export const FIREBASE_MESSAGING_SENDER_ID = "457866718710";
